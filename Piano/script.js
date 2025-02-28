ffd568e5206e527265bca34014684f0df6d6e6ee
Matic Jovan <matic.jovan219@gmail.com>
2025-02-28 23:36:02 +0100

    v1.0.1.

---
+++ Piano/script.js
@@ -0,0 +1,17 @@
+function playSound(note){
+    console.log(`play sound: ${note}`);
+    //play corect mp3 file
+}
+
+const playSound2 = () => {
+    console.log(`play sound: ${note}`);
+}
+
+
+const buttonHTMLCollection = document.getElementsByTagName('button');
+const buttons= Array.from("buttonHTMLCollection");
+const buttons2 = [...buttonHTMLCollection];
+
+
+
+document.getElementsByTagName('button').addEventListener("click", calculate());
