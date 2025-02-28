e506bfe1a490ff71557a115ae6626da758e76fb9
Matic Jovan <matic.jovan219@gmail.com>
2025-03-01 00:54:08 +0100

    v1.1.0

--- Piano/script.js
+++ Piano/script.js
@@ -1,17 +1,32 @@
-function playSound(note){
-    console.log(`play sound: ${note}`);
-    //play corect mp3 file
+const WHITE_KEYS = ['a', 's', 'd', 'f', 'g', 'h', 'j'];
+const BLACK_KEYS = ['w', 'e', 't', 'z', 'u'];
+
+const keys = document.querySelectorAll('.key');
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
+
+keys.forEach(key => {
+    key.addEventListener('click',() => playNote(key));
+    console.log(key);
+})
+
+document.addEventListener('keydown', e => {
+    if(e.repeat) return;
+    const key = e.key;
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+    const blackKeyIndex = BLACK_KEYS.indexOf(key);
+
+    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex]);
+    if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex]);
+
+})
+
+function playNote(key) {
+    const noteAudio = document.getElementById(key.dataset.note);
+    noteAudio.currentTime = 0;
+    noteAudio.play();
+    key.classList.add('active');
+    noteAudio.addEventListener('ended', () => {
+        key.classList.remove('active');
+    })
 }
-
-const playSound2 = () => {
-    console.log(`play sound: ${note}`);
-}
-
-
-const buttonHTMLCollection = document.getElementsByTagName('button');
-const buttons= Array.from("buttonHTMLCollection");
-const buttons2 = [...buttonHTMLCollection];
-
-
-
-document.getElementsByTagName('button').addEventListener("click", calculate());
